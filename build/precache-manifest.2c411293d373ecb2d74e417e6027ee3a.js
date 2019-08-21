18aeb552551c9140d494feb2572c680ff559d106
Ming <lumingabby@outlook.com>
2019-08-21 17:11:04 -0400

    update redirect

--- build/precache-manifest.2c411293d373ecb2d74e417e6027ee3a.js
+++ build/precache-manifest.2c411293d373ecb2d74e417e6027ee3a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3eb4b000975af0b47b774c034ff113b3",
+    "revision": "6b47b26ebced1b49f0da4f6f943892b9",
     "url": "/Projects/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Projects/static/css/2.a471784d.chunk.css"
   },
   {
-    "revision": "58929f62698496a74feb",
+    "revision": "51b1a754573f71e72710",
     "url": "/Projects/static/css/main.fe47a9e2.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Projects/static/js/2.0b8f2399.chunk.js"
   },
   {
-    "revision": "58929f62698496a74feb",
-    "url": "/Projects/static/js/main.dabc2390.chunk.js"
+    "revision": "51b1a754573f71e72710",
+    "url": "/Projects/static/js/main.6a53f936.chunk.js"
   },
   {
     "revision": "7e4c1e50112476b6852f",
